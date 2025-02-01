13ee747e8d12ca0ff8e9764a5ef4519256c30695
idris-is <idrisis1234@gmail.com>
2025-02-01 20:18:06 +0300

    product page düzenleme

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:urun_fiyat/MyFile/String.dart';
 import 'package:urun_fiyat/Pages/urun_kaydetme.dart';
 import 'package:urun_fiyat/widgets/animation.dart';
+import 'product_detail_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -357,70 +358,6 @@ class MyListTile extends StatelessWidget {
             ),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class ProductDetailPage extends StatelessWidget {
-  final Map<String, dynamic> product;
-
-  const ProductDetailPage({Key? key, required this.product}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    String? imageUrl = product['imageUrl'];
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(product['Urun Adi']),
-        backgroundColor: const Color(0xFF48cae4),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const SizedBox(height: 20),
-          Hero(
-            tag: product['id'], // Hero widget ile aynı tag
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: imageUrl != null
-                  ? Image.network(
-                      imageUrl,
-                      width: 200,
-                      height: 200,
-                      fit: BoxFit.cover,
-                    )
-                  : const Image(
-                      image: AssetImage('assets/ic_launcher.png'),
-                      width: 200,
-                      height: 200,
-                    ),
-            ),
-          ),
-          const SizedBox(height: 20),
-          Text(
-            '${MyTexts().adi} ${product['Urun Adi']}',
-            style: const TextStyle(
-                fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 10),
-          Text(
-            '${MyTexts().marka} ${product['Urun Markasi']}',
-            style: const TextStyle(fontSize: 18),
-          ),
-          const SizedBox(height: 10),
-          Text(
-            '${MyTexts().cinsi} ${product['Urun Cinsi']}',
-            style: const TextStyle(fontSize: 18),
-          ),
-          const SizedBox(height: 10),
-          Text(
-            '${MyTexts().fiyat}: ${product['Urun Fiyati']} ${MyTexts().tl}',
-            style: const TextStyle(
-                fontSize: 20, fontWeight: FontWeight.w600),
-          ),
-        ],
       ),
     );
   }
